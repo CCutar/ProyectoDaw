4b613674993244430c6e958b5be8889711114dd6
pererf <pereroca@iesmontsia.org>
2023-11-06 19:56:24 +0100

    test mare de deu

--- DocumentRoot/app/traducciones.php
+++ DocumentRoot/app/traducciones.php
@@ -40,7 +40,7 @@ session_start();
     </style>
 </head>
 <body>
-<div class="sidebar">
+<div class="sidebar sticky-top">
         <h1>Backoffice</h1>
         <div class="info-menu-lateral">
             <img class="logo-menu-lateral" src="https://www.logomaker.com/api/main/images/1j+ojFVDOMkX9Wytexe43D6kh...OJqBBPmhrFwXs1M3EMoAJtliklh...Rs9...8+ " alt="Logo de la empresa" width="95" height="57">
@@ -52,33 +52,29 @@ session_start();
             <li><a href="#"><i class="fas fa-chart-bar"></i> Pedidos</a></li>
             <li><a href="#"><i class="fas fa-users"></i> Productos</a></li>
             <li><a href="#"><i class="fas fa-cog"></i> Reclamaciones</a></li>
-            <li><a href="traducciones.php"><i class="fas fa-cog"></i>Traducciones</a></li>
+            <li><a href="cms.php"><i class="fas fa-cog"></i>Traducciones</a></li>
         </ul>
         <section class="logout">
             <a href="kill.php"><i class="fas fa-cog"></i> LOG OUT</a>
 
         </section>
     </div>
-
-
-    <!-- Contenido principal -->
-    <div class="content" style="margin-left: 20%;">
-        <div class="grid-container">
-    
-            <div class="top-left">
-                <h1>Esquina izquierda top </h1>
-            </div>
-            <div class="top-right">
-                <h1>Esquina derecha top </h1>
-            </div>
-            <div class="bottom-left">
-                <h1>Esquina izquierda bot </h1>
-            </div>
-            <div class="bottom-right">
-                <h1>Esquina derecha bot </h1>
-            </div>
+    <section class="cms">
+        <div class="contenido" style="margin-left: 20%">
+        <div class="header"><h1>Esto es el título</h1></div>
+        <form class="form-cms" action="insertarcms.php" method="post">
+        <h3>Clau api picanova</h3>    
+        <input type="text" id="api_picanova" value="<?php $api_picanova?>" name="api_picanova"></input>
+        <h3>Clau api upscale</h3>
+        <input type="text" id="api_upscale" name="api_upscale"></input>   
+        <h3>Clau api dall-e</h3>
+        <input type="text" id="api_dalle" name="api_dalle"></input>
+        <button href="#" class="submit" id="actualizar-cms">Actualizar valores</button>  
         </div>
-    </div>
+
+
+    </section>
+
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </body>
 </html>
